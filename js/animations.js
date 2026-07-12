e5eda22ad9d46c2986e1c60465e6bc5415d175f9
Vidlyra <funhigh924@gmail.com>
2026-07-12 15:48:48 +0530

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,1 +1,24 @@
 console.log("Animations Loaded");
+const lightning = document.querySelector(".lightning");
+
+function flashLightning(){
+
+    lightning.classList.add("flash");
+
+    setTimeout(()=>{
+        lightning.classList.remove("flash");
+    },350);
+
+}
+
+setInterval(()=>{
+
+    const random=Math.random();
+
+    if(random>0.80){
+
+        flashLightning();
+
+    }
+
+},2500);
