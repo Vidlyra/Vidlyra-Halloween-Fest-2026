e9db7c52add3d4641593e66298266d9b3d4d74a2
Vidlyra <funhigh924@gmail.com>
2026-07-12 15:57:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ enterButton.addEventListener("click", () => {
 alert("Welcome to Vidlyra Halloween Fest 2026!");
 
 });
+const button=document.getElementById("enterButton");
+
+button.addEventListener("mouseenter",()=>{
+
+button.style.transform="scale(1.1)";
+
+});
+
+button.addEventListener("mouseleave",()=>{
+
+button.style.transform="scale(1)";
+
+});
+
+button.addEventListener("click",()=>{
+
+alert("The Haunted Realm is opening...");
+
+});
