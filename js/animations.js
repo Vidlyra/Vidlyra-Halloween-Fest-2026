6dbe3e3f3497698fc19db813ceb20e9bc31a85a5
Vidlyra <funhigh924@gmail.com>
2026-07-13 15:15:26 +0530

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,17 +1,84 @@
-const lightning = document.querySelector(".lightning");
+const thunder=new Audio("assets/audio/thunder.mp3");
 
-function flashLightning(){
+thunder.volume=.35;
+const svg=document.querySelector(".lightning-svg");
+const moon=document.querySelector(".moon");
+const fog=document.querySelectorAll(".fog");
 
-    lightning.classList.add("flash");
+function lightning(){
 
-    setTimeout(()=>{
-        lightning.classList.remove("flash");
-    },800);
+svg.innerHTML="";
+
+const startX=Math.random()*window.innerWidth;
+
+let x=startX;
+
+let y=0;
+
+let path=`M ${x} ${y}`;
+
+for(let i=0;i<10;i++){
+
+x+=Math.random()*80-40;
+
+y+=70;
+
+path+=` L ${x} ${y}`;
+
+}
+
+const bolt=document.createElementNS("http://www.w3.org/2000/svg","path");
+
+bolt.setAttribute("d",path);
+
+bolt.setAttribute("class","lightning-line");
+
+svg.appendChild(bolt);
+
+bolt.animate(
+
+[
+{opacity:0},
+{opacity:1},
+{opacity:.3},
+{opacity:1},
+{opacity:0}
+],
+
+{
+duration:450
+}
+
+);
+
+moon.classList.add("flash");
+
+fog.forEach(f=>f.classList.add("flash"));
+
+document.body.classList.add("shake");
+
+setTimeout(()=>{
+
+moon.classList.remove("flash");
+
+fog.forEach(f=>f.classList.remove("flash"));
+
+document.body.classList.remove("shake");
+
+svg.innerHTML="";
+
+},450);
 
 }
 
 setInterval(()=>{
 
-    flashLightning();
+if(Math.random()>0.55){
 
-},5000);
+lightning();
+thunder.currentTime=0;
+thunder.play();
+
+}
+
+},3500);
